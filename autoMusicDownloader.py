d1591f2e74b97ccc0bd7a8a91a55b95bff10595e
tadatmya <sriharsha.madala@gmail.com>
2020-03-15 20:44:19 -0700

    Fixed the script to use latest googleapiclient.

--- autoMusicDownloader.py
+++ autoMusicDownloader.py
@@ -1,57 +1,39 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import httplib2
-import os
-
-from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 # For calling instantMusic
 from subprocess import call, check_call
 from subprocess import Popen
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
-
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
-
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-    
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir, 'sheets.googleapis.com-python-quickstart.json')
-
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
-    return credentials
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
 
 def open_downloaded_songs_list(filename):
     """
@@ -76,16 +58,11 @@ def call_instant_music(song, destDir):
     return retVal
 
 def main():
-    credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
-            'version=v4')
-    service = discovery.build('sheets', 'v4', http=http,
-                discoveryServiceUrl=discoveryUrl)
+    service = build('sheets', 'v4', credentials=get_credentials())
     
     # Insert your spread sheet ID and the desired data range.
-    spreadsheetId = 
-    rangeName = 'Sheet1!A:I'
+    spreadsheetId = '<sheed_id_here>'
+    rangeName = 'Sheet1!A:N'
 
     result = service.spreadsheets().values().get(
             spreadsheetId=spreadsheetId, range=rangeName).execute()
